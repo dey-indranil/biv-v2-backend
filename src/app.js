f32b85d0f9c110955e91949a906c7dedd4344350
Indranil Dey <indranildey@MacBookPro-2.lan>
2024-12-14 16:06:28 -0800

    CORS

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3000" }));
+const allowedOrigins = [
+    "http://localhost:3000",
+    "https://agreeable-meadow-03b389710.4.azurestaticapps.net",
+  ];
+  
+app.use(cors({
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  }));
+
+
+
+
 app.use(bodyParser.json());
 app.use("/auth", authRoutes);
 
